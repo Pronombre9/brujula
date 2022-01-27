5c0f3af30f576a344ec0165666d7e1ec787e02a2
Pronombre9 <96122451+Pronombre9@users.noreply.github.com>
2022-01-27 13:46:37 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,17 @@
-input.onGesture(Gesture.Shake, function () {
-    led.plot(0, 0)
-    led.toggle(0, 0)
-})
+let pintar_x = 0
 let x = input.acceleration(Dimension.X)
 let y = input.acceleration(Dimension.Y)
+led.plot(0, 0)
 basic.forever(function () {
-	
+    if (x < -1000) {
+        pintar_x = 0
+    } else if (x < -200) {
+        pintar_x = 1
+    } else if (x < 1000) {
+        pintar_x = 4
+    } else if (x < 200) {
+        pintar_x = 3
+    } else {
+    	
+    }
 })
