6790fad36670402a184e62906da320fd9c99f82d
Pronombre9 <96122451+Pronombre9@users.noreply.github.com>
2022-01-27 13:25:45 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,39 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+})
 basic.forever(function () {
 	
 })
