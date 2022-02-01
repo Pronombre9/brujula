25b5909f4f050f199db4152216ba9acecd9a5e28
Pronombre9 <96122451+Pronombre9@users.noreply.github.com>
2022-02-01 13:45:15 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,31 @@
+let x = 0
+let y = 0
 let pintar_x = 0
-let x = input.acceleration(Dimension.X)
-let y = input.acceleration(Dimension.Y)
-led.plot(0, 0)
+let pintar_y = 0
 basic.forever(function () {
+    x = input.acceleration(Dimension.X)
+    y = input.acceleration(Dimension.Y)
     if (x < -1000) {
         pintar_x = 0
     } else if (x < -200) {
         pintar_x = 1
-    } else if (x < 1000) {
+    } else if (x > 1000) {
         pintar_x = 4
-    } else if (x < 200) {
+    } else if (x > 200) {
         pintar_x = 3
     } else {
-    	
+        pintar_x = 2
     }
+    if (y < -1000) {
+        pintar_y = 0
+    } else if (y < -200) {
+        pintar_y = 1
+    } else if (y > 1000) {
+        pintar_y = 4
+    } else if (y > 200) {
+        pintar_y = 3
+    } else {
+        pintar_y = 2
+    }
+    led.plot(pintar_x, pintar_y)
 })
