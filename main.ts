5b2b3308da3a3a0483d732b2f43d6855487795e0
Pronombre9 <96122451+Pronombre9@users.noreply.github.com>
2022-02-01 13:52:06 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,22 +6,22 @@ basic.forever(function () {
     x = input.acceleration(Dimension.X)
     y = input.acceleration(Dimension.Y)
     led.unplot(pintar_x, pintar_y)
-    if (x < -1000) {
+    if (x < -555) {
         pintar_x = 0
     } else if (x < -200) {
         pintar_x = 1
-    } else if (x > 1000) {
+    } else if (x > 555) {
         pintar_x = 4
     } else if (x > 200) {
         pintar_x = 3
     } else {
         pintar_x = 2
     }
-    if (y < -1000) {
+    if (y < -555) {
         pintar_y = 0
     } else if (y < -200) {
         pintar_y = 1
-    } else if (y > 1000) {
+    } else if (y > 555) {
         pintar_y = 4
     } else if (y > 200) {
         pintar_y = 3
